1b24f9f68045aba1368f88c1e283aa25840d9f84
RISHAB SIKKA <sikka.rishab@gmail.com>
2026-03-21 13:39:40 +0530

    Revised Inheritance and Polymorphism

--- oops_concepts/Inheritance&Polymorphism.cpp
+++ oops_concepts/Inheritance&Polymorphism.cpp
@@ -262,114 +262,23 @@ using namespace std;
 // Static polymorphism using function overloading and operator overloading
 // Method overloading
 
-// class manualCar {
-//     private:
-//     string brand;
-//     string model;
-//     bool isEngineOn;
-//     int currentSpeed;
-//     int currentGear;
-
-//     public: 
-//     manualCar(string b, string m){
-//         this -> brand = b;
-//         this -> model = m;
-//         this -> isEngineOn = false;
-//         this -> currentSpeed = 0;  
-//         this -> currentGear = 0;  
-//     }
-
-//     void startEngine() {
-//         isEngineOn = true;
-//         cout << "Engine started for " << brand << " " << model << endl;
-//     }
-
-//     void stopEngine() {
-//         isEngineOn = false;
-//         currentSpeed = 0;
-//         cout << "Engine stopped for " << brand << " " << model << endl;
-//     }
-
-//     void accelerate() {
-//         if(!isEngineOn){
-//             cout << "Cannot accelerate, engine is off." << endl;
-//             return;
-//         }   
-//         currentSpeed += 10;
-//         cout << "Accelerating " << brand << " " << model << " to " << currentSpeed << " km/h" << endl;
-//     }
-
-//     void accelerate(int increment) {
-//         if(!isEngineOn){
-//             cout << "Cannot accelerate, engine is off." << endl;
-//             return;
-//         }   
-//         currentSpeed += increment;
-//         cout << "Accelerating " << brand << " " << model << " to " << currentSpeed << " km/h with increment of " << increment << endl;
-//     }
-
-//     void brake() {
-//         if (currentSpeed > 0) {
-//             currentSpeed -= 10;
-//             cout << "Braking " << brand << " " << model << ", current speed: " << currentSpeed << " km/h" << endl;
-//         } else {
-//             cout << brand << " " << model << " is already stopped." << endl;
-//         }
-//     }
-
-//     void brake(int decrement) {
-//         if (currentSpeed > 0) {
-//             currentSpeed -= decrement;
-//             if(currentSpeed < 0) currentSpeed = 0;
-//             cout << "Braking " << brand << " " << model << ", current speed: " << currentSpeed << " km/h with decrement of " << decrement << endl;
-//         } else {
-//             cout << brand << " " << model << " is already stopped." << endl;
-//         }
-//     }
-
-//     void shiftGear(){
-//         if(!isEngineOn){
-//             cout << "Cannot shift gear, engine is off." << endl;
-//             return;
-//         }
-//         currentGear++;
-//         cout << "Gear shifted to " << currentGear << " for " << brand << " " << model << endl;
-//     }
-// };
-
-// int main(){
-//     manualCar myManualCar("Toyota", "Corolla");
-//     myManualCar.startEngine();
-//     myManualCar.shiftGear();
-//     myManualCar.accelerate();
-//     myManualCar.accelerate(25); // Using overloaded method
-//     myManualCar.brake();
-//     myManualCar.brake(15); // Using overloaded method
-//     myManualCar.stopEngine();
-
-//     return 0;
-// }
-
-
-// Static and Dynamic Polymorphism combined
-
-class Car {
-    protected:
+class manualCar {
+    private:
     string brand;
     string model;
     bool isEngineOn;
     int currentSpeed;
+    int currentGear;
 
     public: 
-    // Cobstructor
-    Car(string b, string m){
+    manualCar(string b, string m){
         this -> brand = b;
         this -> model = m;
         this -> isEngineOn = false;
         this -> currentSpeed = 0;  
+        this -> currentGear = 0;  
     }
 
-    // Common methods 
     void startEngine() {
         isEngineOn = true;
         cout << "Engine started for " << brand << " " << model << endl;
@@ -381,33 +290,7 @@ class Car {
         cout << "Engine stopped for " << brand << " " << model << endl;
     }
 
-    virtual void accelerate() = 0; // Pure virtual function
-
-    virtual void brake() = 0; // Pure virtual function
-
-    // destructor
-    virtual ~Car(){
-        cout << "Car " << brand << " " << model << " is being destroyed." << endl;
-    }
-
-};
-
-class manualCar : public Car {
-    private:
-    int currentGear;
-    public:
-    manualCar(string b, string m) : Car(b,m){
-        this -> currentGear = 0;    
-    }
-    void shiftGear(){
-        if(!isEngineOn){
-            cout << "Cannot shift gear, engine is off." << endl;
-            return;
-        }
-        currentGear++;
-        cout << "Gear shifted to " << currentGear << " for " << brand << " " << model << endl;
-    }
-    void accelerate() override {
+    void accelerate() {
         if(!isEngineOn){
             cout << "Cannot accelerate, engine is off." << endl;
             return;
@@ -415,7 +298,8 @@ class manualCar : public Car {
         currentSpeed += 10;
         cout << "Accelerating " << brand << " " << model << " to " << currentSpeed << " km/h" << endl;
     }
-    void accelerate(int increment) { // Overloaded method
+
+    void accelerate(int increment) {
         if(!isEngineOn){
             cout << "Cannot accelerate, engine is off." << endl;
             return;
@@ -423,7 +307,8 @@ class manualCar : public Car {
         currentSpeed += increment;
         cout << "Accelerating " << brand << " " << model << " to " << currentSpeed << " km/h with increment of " << increment << endl;
     }
-    void brake() override {
+
+    void brake() {
         if (currentSpeed > 0) {
             currentSpeed -= 10;
             cout << "Braking " << brand << " " << model << ", current speed: " << currentSpeed << " km/h" << endl;
@@ -431,7 +316,8 @@ class manualCar : public Car {
             cout << brand << " " << model << " is already stopped." << endl;
         }
     }
-    void brake(int decrement) { // Overloaded method
+
+    void brake(int decrement) {
         if (currentSpeed > 0) {
             currentSpeed -= decrement;
             if(currentSpeed < 0) currentSpeed = 0;
@@ -441,8 +327,15 @@ class manualCar : public Car {
         }
     }
 
+    void shiftGear(){
+        if(!isEngineOn){
+            cout << "Cannot shift gear, engine is off." << endl;
+            return;
+        }
+        currentGear++;
+        cout << "Gear shifted to " << currentGear << " for " << brand << " " << model << endl;
+    }
 };
-
 
 int main(){
     manualCar myManualCar("Toyota", "Corolla");
@@ -456,3 +349,110 @@ int main(){
 
     return 0;
 }
+
+
+// Static and Dynamic Polymorphism combined
+
+// class Car {
+//     protected:
+//     string brand;
+//     string model;
+//     bool isEngineOn;
+//     int currentSpeed;
+
+//     public: 
+//     // Cobstructor
+//     Car(string b, string m){
+//         this -> brand = b;
+//         this -> model = m;
+//         this -> isEngineOn = false;
+//         this -> currentSpeed = 0;  
+//     }
+
+//     // Common methods 
+//     void startEngine() {
+//         isEngineOn = true;
+//         cout << "Engine started for " << brand << " " << model << endl;
+//     }
+
+//     void stopEngine() {
+//         isEngineOn = false;
+//         currentSpeed = 0;
+//         cout << "Engine stopped for " << brand << " " << model << endl;
+//     }
+
+//     virtual void accelerate() = 0; // Pure virtual function
+
+//     virtual void brake() = 0; // Pure virtual function
+
+//     // destructor
+//     virtual ~Car(){
+//         cout << "Car " << brand << " " << model << " is being destroyed." << endl;
+//     }
+
+// };
+
+// class manualCar : public Car {
+//     private:
+//     int currentGear;
+//     public:
+//     manualCar(string b, string m) : Car(b,m){
+//         this -> currentGear = 0;    
+//     }
+//     void shiftGear(){
+//         if(!isEngineOn){
+//             cout << "Cannot shift gear, engine is off." << endl;
+//             return;
+//         }
+//         currentGear++;
+//         cout << "Gear shifted to " << currentGear << " for " << brand << " " << model << endl;
+//     }
+//     void accelerate() override {
+//         if(!isEngineOn){
+//             cout << "Cannot accelerate, engine is off." << endl;
+//             return;
+//         }   
+//         currentSpeed += 10;
+//         cout << "Accelerating " << brand << " " << model << " to " << currentSpeed << " km/h" << endl;
+//     }
+//     void accelerate(int increment) { // Overloaded method
+//         if(!isEngineOn){
+//             cout << "Cannot accelerate, engine is off." << endl;
+//             return;
+//         }   
+//         currentSpeed += increment;
+//         cout << "Accelerating " << brand << " " << model << " to " << currentSpeed << " km/h with increment of " << increment << endl;
+//     }
+//     void brake() override {
+//         if (currentSpeed > 0) {
+//             currentSpeed -= 10;
+//             cout << "Braking " << brand << " " << model << ", current speed: " << currentSpeed << " km/h" << endl;
+//         } else {
+//             cout << brand << " " << model << " is already stopped." << endl;
+//         }
+//     }
+//     void brake(int decrement) { // Overloaded method
+//         if (currentSpeed > 0) {
+//             currentSpeed -= decrement;
+//             if(currentSpeed < 0) currentSpeed = 0;
+//             cout << "Braking " << brand << " " << model << ", current speed: " << currentSpeed << " km/h with decrement of " << decrement << endl;
+//         } else {
+//             cout << brand << " " << model << " is already stopped." << endl;
+//         }
+//     }
+
+// };
+
+
+// int main(){
+//     manualCar myManualCar("Toyota", "Corolla");
+//     myManualCar.startEngine();
+//     myManualCar.shiftGear();
+//     myManualCar.accelerate();
+//     myManualCar.accelerate(25); // Using overloaded method
+//     myManualCar.brake();
+//     myManualCar.brake(15); // Using overloaded method
+//     myManualCar.stopEngine();
+
+//     return 0;
+// }
